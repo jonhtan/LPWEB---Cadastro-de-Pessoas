acf924704223d5916e8d67ba496f91f1c014011d
Jonhtan Mota <jmtrs@rede.ulbra.br>
2020-03-13 08:30:27 -0300

    improve: melhoria na classe person e inicio na função updateStatistic()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,41 +14,67 @@ export class AppComponent {
   people = [];
 
   text = "";
-  agetxt = "";
+  age = "";
   younger = new Person(); 
   older = new Person();
 
 
   register() {
-    this.person.name = this.text;
-    this.person.age = this.agetxt;
+    this.person.setName(this.text);
+    this.person.setAge(parseInt(this.age));
     this.people.push(this.person);
-    this.text = this.agetxt = "";
-    this.person = new Person();
+    this.text = "";
+    this.age = "";
     this.updateStatistic();
+    this.person = new Person();
   }
 
   delete(pessoa) {
     this.people.splice(this.people.indexOf(pessoa), 1);
-    this.updateStatistic();
+    // this.updateStatistic();
   }
 
   updateStatistic() {
-    for(let x in this.people) {
-      for(let y in this.people) {
-        if(this.people[x].age<this.people[y].age) {
-          this.older = this.people[y];
-          this.younger = this.people[x];
-          console.log(this.people[x].age,this.people[y]);
-        }
-      }
+    if(this.younger.getName()) {
+      
+    } else { //if not exist set the first
+      this.younger = this.person;
+      this.older = this.person;
     }
+    // for(let x in this.people) {
+    //   for(let y in this.people) {
+    //     let a1 = this.people[x].age;
+    //     let a2 = this.people[y].age;
+    //     if(a1<a2) {
+    //       this.older = this.people[y];
+    //       this.younger = this.people[x];
+    //       console.log(typeof(a1));
+    //     }
+    //   }
+    // }
   }
 
 }
 
 export class Person {
-  name;
-  age;
+  private name: string;
+  private age: number;
+
+  setName(name: string) {
+    this.name = name;
+  }
+
+  setAge(age: number) {
+    this.age = age;
+  }
+
+  getName(): string {
+    return this.name;
+  }
+
+  getAge(): string {
+    return this.age.toString();
+  }
+
 }
 
